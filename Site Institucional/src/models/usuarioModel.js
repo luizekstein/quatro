c29f0392b101fde3bbc24b2793c7563c21572f0e
diegosvg01 <diego.guimaraes@bandtec.com.br>
2022-04-01 21:45:50 -0300

    criando arquivo responsável por realizar as consultas no banco de dados

---
+++ Site Institucional/src/models/usuarioModel.js
@@ -0,0 +1,24 @@
+var database = require("../database/config")
+
+function entrar(cnpj, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", cnpj, senha)
+    var instrucao = `
+        SELECT * FROM cliente WHERE cnpj = '${cnpj}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function cadastrar(cnpj ,email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", cnpj, email, senha);
+    var instrucao = `
+        INSERT INTO cliente (cnpj, email, senha) VALUES ('${cnpj}', '${email}', '${senha}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+module.exports = {
+    entrar,
+    cadastrar,
+};
