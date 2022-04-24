52d4fd20eb7dcd9f5120ec11c3989346287289da
Diego Guimarães <diego.guimaraes@valemobi.com.br>
2022-04-24 12:22:16 -0300

    Configurando a conexão da api looca com o banco de dados local e na nuvem

---
+++ API-JAVA/quatro/src/main/java/com/mycompany/quatro/measurement/Insertion.java
@@ -0,0 +1,54 @@
+package com.mycompany.quatro.measurement;
+
+import com.mycompany.quatro.connection.azure.ConnectionMysql;
+import com.mycompany.quatro.connection.azure.ConnectionSqlServer;
+
+
+public class Insertion {
+    ConnectionMysql mysql = new ConnectionMysql();
+    ConnectionSqlServer mssql = new ConnectionSqlServer();
+
+
+    public void diskMeasurementInsertion(Long usage, String date, String uuid) {
+//        mysql.getMysql().update(
+//                "INSERT INTO " +
+//                        "measurement(usage, measurement_date, fk_component, usage_unit) " +
+//                    "VALUES" +
+//                        "(?, ?, ?, 'B')", usage, date, component);
+
+        mssql.getMssql().update(
+                "INSERT INTO " +
+                        "measurement(usage, measurement_date, fk_component, usage_unit) " +
+                        "VALUES" +
+                        "(?, ?, (select id_component from [dbo].[component] where UUID = '" + uuid + "'), 'B')", usage, date);
+    }
+
+    public void cpuMeasurementInsertion(Long usage, Double temperature, String date, Integer component) {
+//        mysql.getMysql().update(
+//                "INSERT INTO " +
+//                        "measurement(usage, temperature, measurement_date, fk_component, usage_unit, temperature_unit) " +
+//                        "VALUES" +
+//                        "(?, ?, ?, ?, '%', 'ºC')", usage, temperature, date, component);
+
+        mssql.getMssql().update(
+                "INSERT INTO " +
+                        "measurement(usage, temperature, measurement_date, fk_component, usage_unit, temperature_unit) " +
+                        "VALUES" +
+                        "(?, ?, ?, ?, '%', 'ºC')", usage, temperature, date, component);
+    }
+
+    public void memoryMeasurementInsertion(Long usage, String date, Integer component) {
+//        mysql.getMysql().update(
+//                "INSERT INTO " +
+//                        "measurement(usage, measurement_date, fk_component, usage_unit) " +
+//                        "VALUES" +
+//                        "(?, ?, ?, 'B')", usage, date, component);
+
+        mssql.getMssql().update(
+                "INSERT INTO " +
+                        "measurement(usage, measurement_date, fk_component, usage_unit) " +
+                        "VALUES" +
+                        "(?, ?, ?, 'B')", usage, date, component);
+    }
+
+}
--- API-JAVA/quatro/src/main/java/com/mycompany/quatro/measurement/Main.java
+++ API-JAVA/quatro/src/main/java/com/mycompany/quatro/measurement/Main.java
@@ -1,21 +1,46 @@
 package com.mycompany.quatro.measurement;
 
 import com.github.britooo.looca.api.core.Looca;
-
+import com.github.britooo.looca.api.group.temperatura.Temperatura;
+import oshi.SystemInfo;
+import oshi.software.os.FileSystem;
+import oshi.software.os.OSFileStore;
+import oshi.software.os.OperatingSystem;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Main {
     public static void main(String[] args) {
+        SystemInfo systemInfo = new SystemInfo();
+        OperatingSystem operatingSystem = systemInfo.getOperatingSystem();
+        FileSystem fileSystem = operatingSystem.getFileSystem();
+        List<OSFileStore> osFileStores = fileSystem.getFileStores();
         Looca looca = new Looca();
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        Insertion insertion = new Insertion();
+        Temperatura temperatura = new Temperatura();
 
         new Timer().scheduleAtFixedRate(new TimerTask(){
             @Override
             public void run(){
-                System.out.println(String.format("%.2f", looca.getProcessador().getUso()));
+                insertion.cpuMeasurementInsertion(Math.round(looca.getProcessador().getUso()), temperatura.getTemperatura(), dtf.format(now), 1);
+                System.out.println(String.format("Uso processador: %.2f", looca.getProcessador().getUso()));
+                System.out.println("Frequência do processador: " + looca.getProcessador().getFrequencia());
+                System.out.println("Temperatura do processador: " + looca.getTemperatura());
+                insertion.memoryMeasurementInsertion(10073583616L, dtf.format(now), 2);
                 System.out.println(String.format("Memória: %d", looca.getMemoria().getEmUso()));
-                System.out.println(String.format("Disco: %s", looca.getGrupoDeDiscos().getVolumes()));
+                for(OSFileStore fileStore : osFileStores) {
+                    insertion.diskMeasurementInsertion((fileStore.getTotalSpace() - fileStore.getFreeSpace()), dtf.format(now), fileStore.getUUID());
+                    System.out.println("UUID: " + fileStore.getUUID());
+                    System.out.println("Espaço disponível: " + fileStore.getFreeSpace());
+                    System.out.println("Espaço total: " + fileStore.getTotalSpace());
+                    System.out.println("Espaço usado:" + (fileStore.getTotalSpace() - fileStore.getFreeSpace()));
+                }
             }
-        },0,3000);
+        },0,5000);
     }
 }
