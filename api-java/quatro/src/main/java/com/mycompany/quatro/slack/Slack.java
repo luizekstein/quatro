a3cdd0a3b04c513dbd668122ef58d257a7c8fdf3
Kaua De Oliveira Santos Silva <kosilva1@stefanini.com>
2022-05-09 17:12:53 -0300

    slack integrado

--- api-java/quatro/src/main/java/com/mycompany/quatro/slack/Slack.java
+++ api-java/quatro/src/main/java/com/mycompany/quatro/slack/Slack.java
@@ -1,5 +1,26 @@
 package com.mycompany.quatro.slack;
 
-public class Slack {
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import org.json.JSONObject;
 
+
+public class Slack {
+     private static final HttpClient client = HttpClient.newHttpClient();
+    private static final String URL = "https://hooks.slack.com/services/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";
+    
+    public static void sendMessage(JSONObject content)throws IOException, InterruptedException{
+         HttpRequest request = HttpRequest.newBuilder(
+                URI.create(URL))
+                .header("accept", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(content.toString()))
+                .build();
+         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+         
+          System.out.println(String.format("Status: %s", response.statusCode()));
+        System.out.println(String.format("Response: %s", response.body()));
+    }
 }
