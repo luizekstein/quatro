caf062c88071106a154b668c840383deba931431
Kaua De Oliveira Santos Silva <kosilva1@stefanini.com>
2022-05-16 20:20:34 -0300

    logs com txt (esta só esboçado, falta configurar com a nossa regra de negocio)

--- API-JAVA/quatro/src/main/java/com/mycompany/quatro/slack/App.java
+++ API-JAVA/quatro/src/main/java/com/mycompany/quatro/slack/App.java
@@ -5,13 +5,13 @@ import java.io.IOException;
 import org.json.JSONObject;
 
 
+
 public class App {
     public static void main(String[] args) throws IOException, InterruptedException {
         
+        
         JSONObject json = new JSONObject();
-        
-        json.put("text", "Boa tarde Kauã, como posso te ajudar? :shrug:");
-        
+        json.put("text", "Alerta: Servidor moderadamente cheio");
         Slack.sendMessage(json);
     }
 }
